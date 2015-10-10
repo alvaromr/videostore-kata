05e13e572a8efcbd17e55c3dca1178468345a015
alvaromr <alvaro.martin.rodriguez@alumnos.upm.es>
2015-10-10 13:17:14 +0200

    Rename variables in amountFor

--- src/kata/Customer.java
+++ src/kata/Customer.java
@@ -12,8 +12,6 @@ class Customer {
         this.name = name;
     }
 
-    ;
-
     public void addRental(Rental arg) {
         rentals.addElement(arg);
     }
@@ -21,8 +19,6 @@ class Customer {
     public String getName() {
         return name;
     }
-
-    ;
 
     public String statement() {
         double totalAmount = 0;
@@ -52,25 +48,25 @@ class Customer {
         return result;
     }
 
-    private double amountFor(Rental each) {
-        double thisAmount = 0.0;
+    private double amountFor(Rental aRental) {
+        double result = 0.0;
         // determine amounts for each line
-        switch (each.getMovie().getPriceCode()) {
+        switch (aRental.getMovie().getPriceCode()) {
             case Movie.REGULAR:
-                thisAmount += 2;
-                if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                result += 2;
+                if (aRental.getDaysRented() > 2)
+                    result += (aRental.getDaysRented() - 2) * 1.5;
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
+                result += aRental.getDaysRented() * 3;
                 break;
             case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                result += 1.5;
+                if (aRental.getDaysRented() > 3)
+                    result += (aRental.getDaysRented() - 3) * 1.5;
                 break;
         }
-        return thisAmount;
+        return result;
     }
 }
 
