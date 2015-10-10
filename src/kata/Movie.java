ac0d37e4da257e25d906c63ac8363acd11c5ba32
alvaromr <alvaro.martin.rodriguez@alumnos.upm.es>
2015-10-10 14:20:41 +0200

    Move getFrequentRenterPoints to Price

--- src/kata/Movie.java
+++ src/kata/Movie.java
@@ -41,10 +41,6 @@ public class Movie {
     }
 
     int getFrequentRenterPoints(int daysRented) {
-        if ((this.getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
-            return 2;
-        } else {
-            return 1;
-        }
+        return this.price.getFrequentRenterPoints(daysRented);
     }
 }
--- src/kata/Price.java
+++ src/kata/Price.java
@@ -4,4 +4,12 @@ abstract class Price {
     abstract int getPriceCode();
 
     abstract double getCharge(int daysRented);
+
+    int getFrequentRenterPoints(int daysRented) {
+        if ((this.getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
 }
