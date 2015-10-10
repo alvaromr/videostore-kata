7fbcf4ba66036b051f32bc7f607d28ca8af17c77
alvaromr <alvaro.martin.rodriguez@alumnos.upm.es>
2015-10-10 13:56:40 +0200

    Move getCharge to Movie

--- src/kata/Rental.java
+++ src/kata/Rental.java
@@ -18,24 +18,7 @@ class Rental {
     }
 
     double getCharge() {
-        double result = 0.0;
-        // determine amounts for each line
-        switch (this.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (this.getDaysRented() > 2)
-                    result += (this.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += this.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (this.getDaysRented() > 3)
-                    result += (this.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
+        return this.getMovie().getCharge(this.daysRented);
     }
 
     int getFrequentRenterPoints() {
