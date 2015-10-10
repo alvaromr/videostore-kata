645074eba122c574475d4f94421ec26876eb2b16
alvaromr <alvaro.martin.rodriguez@alumnos.upm.es>
2015-10-10 13:36:13 +0200

    Move frequent renter points to Rental

--- src/kata/Rental.java
+++ src/kata/Rental.java
@@ -37,4 +37,12 @@ class Rental {
         }
         return result;
     }
+
+    int getFrequentRenterPoints() {
+        if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) && this.getDaysRented() > 1) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
 }
