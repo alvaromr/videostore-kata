e4e31d66c8869cb19731f493c7816440ae94c53b
alvaromr <alvaro.martin.rodriguez@alumnos.upm.es>
2015-10-10 13:21:22 +0200

    Move amountFor to Rental and rename to getCharge

--- src/kata/Customer.java
+++ src/kata/Customer.java
@@ -29,7 +29,7 @@ class Customer {
             double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
             // determine amounts for each line
-            thisAmount = amountFor(each);
+            thisAmount = each.getCharge();
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
@@ -45,27 +45,6 @@ class Customer {
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints)
                 + " frequent renter points";
-        return result;
-    }
-
-    private double amountFor(Rental aRental) {
-        double result = 0.0;
-        // determine amounts for each line
-        switch (aRental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (aRental.getDaysRented() > 2)
-                    result += (aRental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += aRental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (aRental.getDaysRented() > 3)
-                    result += (aRental.getDaysRented() - 3) * 1.5;
-                break;
-        }
         return result;
     }
 }
